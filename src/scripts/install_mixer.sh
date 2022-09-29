2050f7f4d5a542010d13b53c3039a997af7a994b
Oleksandr Frei <oleksandr.frei@gmail.com>
2022-09-29 13:12:16 +0000

    scripts needed to build mixer.sif container

---
+++ src/scripts/install_mixer.sh
@@ -0,0 +1,9 @@
+cd /tools
+wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
+tar -xzvf boost_1_69_0.tar.gz && cd boost_1_69_0
+./bootstrap.sh --with-libraries=program_options,filesystem,system,date_time
+./b2 --clean && ./b2 --j12 -a
+cd /tools
+git clone --recurse-submodules -j8 https://github.com/precimed/mixer.git
+mkdir mixer/src/build && cd mixer/src/build && cmake .. -DBoost_NO_BOOST_CMAKE=ON -DBOOST_ROOT=/tools/boost_1_69_0 && make -j16 bgmg
+	
