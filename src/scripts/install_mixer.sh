a439ebcbccaebe0ee274b0f59344a9d1cdd735cb
Oleksandr Frei <oleksandr.frei@gmail.com>
2022-09-29 13:12:28 +0000

    fix boost library downloads

--- src/scripts/install_mixer.sh
+++ src/scripts/install_mixer.sh
@@ -1,5 +1,5 @@
 cd /tools
-wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
+wget https://boostorg.jfrog.io/artifactory/main/release/1.69.0/source/boost_1_69_0.tar.gz
 tar -xzvf boost_1_69_0.tar.gz && cd boost_1_69_0
 ./bootstrap.sh --with-libraries=program_options,filesystem,system,date_time
 ./b2 --clean && ./b2 --j12 -a
