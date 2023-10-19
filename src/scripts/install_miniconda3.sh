fb56e260eaba58f49d85c29a52405213d9a5ffa5
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-10-19 06:48:43 +0000

    pin versions; ubuntu 18.04->20.04

--- src/scripts/install_miniconda3.sh
+++ src/scripts/install_miniconda3.sh
@@ -1,4 +1,4 @@
-curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/miniconda3.sh \
+curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-py38_23.5.1-0-Linux-x86_64.sh -o /tmp/miniconda3.sh \
   && mkdir /root/.conda \
   && bash /tmp/miniconda3.sh -bfp /usr/local \
   && rm -rf /tmp/miniconda3.sh
